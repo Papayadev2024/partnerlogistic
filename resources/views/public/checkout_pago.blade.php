302c7f899fbafd50e25f7a3120340f58d571f7b5
Carlos Colina <carlosecolina89@gmail.com>
2024-05-07 15:35:33 -0500

    actualizacion y mejoras

--- resources/views/public/checkout_pago.blade.php
+++ resources/views/public/checkout_pago.blade.php
@@ -274,11 +274,19 @@
                         </div>
 
                         <div class="flex flex-col gap-2">
-                          <label for="numero_tarjeta" class="font-medium text-[12px] text-[#6C7275]">Número de
-                            tarjeta</label>
-                          <input id="numero_tarjeta" name="numero_tarjeta" type="text"
-                            placeholder="1234 12345 1234"
-                            class="w-full py-3 px-4 focus:outline-none placeholder-gray-400 font-normal text-[16px] border-[1.5px] border-gray-200 rounded-xl" />
+                          <div class=" md:w-1/2">
+                            <label for="numero_tarjeta" class="font-medium text-[12px] text-[#6C7275]">
+                              Número de
+                              tarjeta</label>
+                            <input id="numero_tarjeta" name="numero_tarjeta" type="text"
+                              placeholder="1234 12345 1234"
+                              class="w-full py-3 px-4 focus:outline-none placeholder-gray-400 font-normal text-[16px] border-[1.5px] border-gray-200 rounded-xl" />
+
+
+                          </div>
+                          <div id="iconoTarjeta">
+
+                          </div>
                         </div>
 
                         <div class="flex flex-col md:flex-row gap-5">
@@ -303,10 +311,10 @@
                       <a href="/agradecimiento" id="pagarProductos"
                         class="text-white bg-[#74A68D] w-full py-3 rounded-3xl cursor-pointer border-2 font-semibold text-[16px] inline-block text-center border-none">Pagar</a>
                       <!-- <input
-                                                                                                                                                  type="submit"
-                                                                                                                                                  value="Checkout"
-                                                                                                                                                  class="text-white bg-[#74A68D] w-full py-3 rounded-3xl cursor-pointer border-2 font-semibold text-[16px] inline-block text-center border-none"
-                                                                                                                                                /> -->
+                                                                                                                                                                                                    type="submit"
+                                                                                                                                                                                                    value="Checkout"
+                                                                                                                                                                                                    class="text-white bg-[#74A68D] w-full py-3 rounded-3xl cursor-pointer border-2 font-semibold text-[16px] inline-block text-center border-none"
+                                                                                                                                                                                                  /> -->
                     </div>
 
                   </div>
@@ -358,6 +366,43 @@
 
 
 @section('scripts_importados')
+  <script>
+    const svgVisa = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48">
+                    <path fill="#1565C0" d="M45,35c0,2.209-1.791,4-4,4H7c-2.209,0-4-1.791-4-4V13c0-2.209,1.791-4,4-4h34c2.209,0,4,1.791,4,4V35z"></path><path fill="#FFF" d="M15.186 19l-2.626 7.832c0 0-.667-3.313-.733-3.729-1.495-3.411-3.701-3.221-3.701-3.221L10.726 30v-.002h3.161L18.258 19H15.186zM17.689 30L20.56 30 22.296 19 19.389 19zM38.008 19h-3.021l-4.71 11h2.852l.588-1.571h3.596L37.619 30h2.613L38.008 19zM34.513 26.328l1.563-4.157.818 4.157H34.513zM26.369 22.206c0-.606.498-1.057 1.926-1.057.928 0 1.991.674 1.991.674l.466-2.309c0 0-1.358-.515-2.691-.515-3.019 0-4.576 1.444-4.576 3.272 0 3.306 3.979 2.853 3.979 4.551 0 .291-.231.964-1.888.964-1.662 0-2.759-.609-2.759-.609l-.495 2.216c0 0 1.063.606 3.117.606 2.059 0 4.915-1.54 4.915-3.752C30.354 23.586 26.369 23.394 26.369 22.206z"></path><path fill="#FFC107" d="M12.212,24.945l-0.966-4.748c0,0-0.437-1.029-1.573-1.029c-1.136,0-4.44,0-4.44,0S10.894,20.84,12.212,24.945z"></path>
+                    </svg>`
+
+    const svgMasterCar = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 48 48">
+                        <path fill="#3F51B5" d="M45,35c0,2.209-1.791,4-4,4H7c-2.209,0-4-1.791-4-4V13c0-2.209,1.791-4,4-4h34c2.209,0,4,1.791,4,4V35z"></path><path fill="#FFC107" d="M30 14A10 10 0 1 0 30 34A10 10 0 1 0 30 14Z"></path><path fill="#FF3D00" d="M22.014,30c-0.464-0.617-0.863-1.284-1.176-2h5.325c0.278-0.636,0.496-1.304,0.637-2h-6.598C20.07,25.354,20,24.686,20,24h7c0-0.686-0.07-1.354-0.201-2h-6.598c0.142-0.696,0.359-1.364,0.637-2h5.325c-0.313-0.716-0.711-1.383-1.176-2h-2.973c0.437-0.58,0.93-1.122,1.481-1.595C21.747,14.909,19.481,14,17,14c-5.523,0-10,4.477-10,10s4.477,10,10,10c3.269,0,6.162-1.575,7.986-4H22.014z"></path>
+                        </svg>`
+
+    function obtnerSvg(input) {
+      const visaPattern = /^4/;
+      const mastercardPattern = /^5/;
+      const amexPattern = /^3/;
+      const discoverPattern = /^6/;
+
+      if (visaPattern.test(input)) {
+        return svgVisa;
+      } else if (mastercardPattern.test(input)) {
+        return svgMasterCar;
+      } else if (amexPattern.test(input)) {
+        return "American Express";
+      } else if (discoverPattern.test(input)) {
+        return "Discover";
+      } else {
+        return "Tipo de tarjeta no reconocido";
+      }
+
+    }
+    $('#numero_tarjeta').on('input', function() {
+      let input = $('#numero_tarjeta').val()
+
+      let svg = obtnerSvg(input)
+      $('#iconoTarjeta').html(svg)
+
+
+    })
+  </script>
   <script>
     $('#pagarProductos').on('click', function(e) {
       console.log('pagando servicio');
@@ -701,6 +746,7 @@
       }
     })
   </script>
+
 
   <script src="{{ asset('js/storage.extend.js') }}"></script>
 @stop
